BKertesz/unix-history-repo

commit 4cd8ae282db8cd244cb2b8077e6181a4feb9a442

Replace an unreachable panic() in vm86_getptr (been there for 13 years) with a KASSERT() behind the functions's only consumer.
Suggested by:	kib
Reviewed by:	kib
CID:		4494
Found with:	Coverity Prevent(tm)
MFC after:	2 weeks


git-svn-id: svn+ssh://svn.freebsd.org/base/head@237924 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/i386/i386/machdep.c
@@ -2176,7 +2176,7 @@ basemem_setup(void)
 static void
 getmemsize(int first)
 {
-	int has_smap, off, physmap_idx, pa_indx, da_indx;
+	int has_smap, off, physmap_idx, pa_indx, da_indx, res;
 	u_long physmem_tunable, memtest;
 	vm_paddr_t physmap[PHYSMAP_SIZE];
 	pt_entry_t *pte;
@@ -2267,7 +2267,8 @@ getmemsize(int first)
 	pmap_kenter(KERNBASE + (1 << PAGE_SHIFT), 1 << PAGE_SHIFT);
 	vmc.npages = 0;
 	smap = (void *)vm86_addpage(&vmc, 1, KERNBASE + (1 << PAGE_SHIFT));
-	vm86_getptr(&vmc, (vm_offset_t)smap, &vmf.vmf_es, &vmf.vmf_di);
+	res = vm86_getptr(&vmc, (vm_offset_t)smap, &vmf.vmf_es, &vmf.vmf_di);
+	KASSERT(res != 0, ("vm86_getptr() failed: address not found"));
 
 	vmf.vmf_ebx = 0;
 	do {

sys/i386/i386/vm86.c
@@ -650,7 +650,6 @@ vm86_getptr(vmc, kva, sel, off)
 			return (1);
 		}
 	return (0);
-	panic("vm86_getptr: address not found");
 }
 	
 int

commit 0373d10b987b62766d0ddacda14f6502a783d4a2

Fix XEN build, broken in r237924.
Reported by:	gcooper
Pointy hat:	brueffer

sys/i386/i386/machdep.c
@@ -2176,13 +2176,13 @@ basemem_setup(void)
 static void
 getmemsize(int first)
 {
-	int has_smap, off, physmap_idx, pa_indx, da_indx, res;
+	int has_smap, off, physmap_idx, pa_indx, da_indx;
 	u_long physmem_tunable, memtest;
 	vm_paddr_t physmap[PHYSMAP_SIZE];
 	pt_entry_t *pte;
 	quad_t dcons_addr, dcons_size;
 #ifndef XEN
-	int hasbrokenint12, i;
+	int hasbrokenint12, i, res;
 	u_int extmem;
 	struct vm86frame vmf;
 	struct vm86context vmc;