BKertesz/unix-history-repo

commit 3165ab95e9c0a36772bddf25ff1f36951c3f0d97

Add a WITH_CLANG_EXTRAS option for src.conf(5), disabled by default, that builds the following additional llvm/clang tools:
- bugpoint
- llc
- lli
- llvm-ar
- llvm-as
- llvm-bcanalyzer
- llvm-diff
- llvm-dis
- llvm-extract
- llvm-ld
- llvm-link
- llvm-mc
- llvm-nm
- llvm-objdump
- llvm-prof
- llvm-ranlib
- llvm-rtdyld
- llvm-stub
- macho-dump
- opt

These tools are mainly useful for people that want to manipulate llvm
bitcode (.bc) and llvm assembly language (.ll) files, or want to tinker
with llvm and clang themselves.

MFC after:	2 weeks


git-svn-id: svn+ssh://svn.freebsd.org/base/head@231057 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

share/mk/bsd.own.mk
@@ -418,6 +418,7 @@ __DEFAULT_NO_OPTIONS = \
     BIND_LIBS \
     BIND_SIGCHASE \
     BIND_XML \
+    CLANG_EXTRAS \
     CTF \
     HESIOD \
     ICONV \
@@ -522,6 +523,10 @@ MK_SOURCELESS_UCODE:= no
 .if ${MK_CDDL} == "no"
 MK_ZFS:=	no
 MK_CTF:=	no
+.endif
+
+.if ${MK_CLANG} == "no"
+MK_CLANG_EXTRAS:= no
 .endif
 
 .if ${MK_CRYPT} == "no"

commit 03ffcea738a94d846f9b580f5118e21c70bfb29d

Fix a problem introduced in r231057: in bsd.own.mk, move the test for whether clang is enabled to just after the last place where it could have been forced to "no".
MFC after:	3 days

share/mk/bsd.own.mk
@@ -481,10 +481,6 @@ MK_ZFS:=	no
 MK_CTF:=	no
 .endif
 
-.if ${MK_CLANG} == "no"
-MK_CLANG_EXTRAS:= no
-.endif
-
 .if ${MK_CRYPT} == "no"
 MK_OPENSSL:=	no
 MK_OPENSSH:=	no
@@ -527,6 +523,7 @@ MK_GDB:=	no
 .endif
 
 .if ${MK_CLANG} == "no"
+MK_CLANG_EXTRAS:= no
 MK_CLANG_IS_CC:= no
 .endif
 